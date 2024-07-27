b57b6a988f41ff6d58eca5ef01b069298ae760e8
ilyag26 <gayvoronskiy.i02@gmail.com>
2024-07-27 22:38:38 +0200

    receiving a specific currency

--- main.py
+++ main.py
@@ -2,66 +2,67 @@ from flask import Flask, request, jsonify
 from waitress import serve
 import sqlite3
 
+def formate_data(data):
+    ar = []
+    for i in range(0, len(data)):
+        date = {
+            "id" : data[i][0],
+            "code" : data[i][1],
+            "fullname" : data[i][2],
+            "sign" : data[i][3]
+        }
+        ar.append(date)
+    return ar
+
 class Db:
     def __init__(self, db_file):
         self.conn = sqlite3.connect(db_file)
         self.cursor = self.conn.cursor()
 
     def show_currency(self):
-
-        self.conn.row_factory = sqlite3.Row 
-        users = self.cursor.execute("SELECT * FROM `Currencies`")
-        return users.fetchall()
-
-    # def add_records(self,user_id, user_balance):
-    #     result = self.cursor.execute("UPDATE `currency` SET `user_balance` = ? WHERE `user_id` = ?",(user_balance,user_id))
-    #     return self.conn.commit()
+        currency = self.cursor.execute(f"SELECT * FROM `Currencies`")
+        return formate_data(currency.fetchall())
+    
+    def show_currency_url(self, code):
+        currency = self.cursor.execute(f"SELECT * FROM `Currencies` WHERE code = ?", (code,))
+        return formate_data(currency.fetchall())
+    
+    #   self.cursor.execute("UPDATE `currency` SET `user_balance` = ? WHERE `user_id` = ?",(user_balance,user_id))
     
     def add_currency(self, code, fullname, sign):
         result = self.cursor.execute("INSERT INTO `Currencies` (`code`,`fullname`, `sign`) VALUES (?,?,?)",(code,fullname,sign))
         return self.conn.commit()
-
-    # def show_records(self):
-    #     users = self.cursor.execute("SELECT * FROM `records`")
-    #     return users.fetchall()
 
     def close(self):
         self.conn.close()
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def error(error):
+    return "Page not found", 404
+
 #http://localhost:8080/currency_add?code=USD&fullname=United%20States%20dollar&sign=$
 @app.route('/currency_add', methods=['POST'])
-def index():
+def currency_add():
     Db1 = Db("currency.db")
     code = request.args.get('code')
     fullname = request.args.get('fullname')
     sign = request.args.get('sign')
     Db1.add_currency(code,fullname,sign)
-    return "Success", 200
+    return Db1.show_currency_url(code), 200
 
 #http://localhost:8080/currency
-@app.route('/currency', methods=['POST'])
+@app.route('/currency', methods=['GET'])
 def currency():
     Db1 = Db("currency.db")
-    ar = []
-    for i in range(0, len(Db1.show_currency())):
-        date = {
-            "id" : Db1.show_currency()[i][0],
-            "code" : Db1.show_currency()[i][1],
-            "fullname" : Db1.show_currency()[i][2],
-            "sign" : Db1.show_currency()[i][3]
-        }
-        ar.append(date)
-    return jsonify(ar)
+    return jsonify(Db1.show_currency()), 200
 
-# for index in Db.show_users():
-#     print(index)
-# print("---------")
-# for index in Db.show_records():
-#     print(index)
-# Db.add_user(3232321,32388383)
-# Db.edit_user(3232321,999999999)
-# Db.add_records(334827647, 987887)
+#http://localhost:8080/currency/EUR
+@app.route('/currency/<code>', methods=['GET'])
+def currency_path(code):
+    Db1 = Db("currency.db")
+    return jsonify(Db1.show_currency_url(code)) if Db1.show_currency_url(code) else "Currency not exist", 200 if Db1.show_currency_url(code) else 404
+
 
 serve(app, host="localhost", port=8080)
