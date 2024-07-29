a14033339e4626a268cf9b4ffeae2660ffdf8889
ilyag26 <gayvoronskiy.i02@gmail.com>
2024-07-29 12:47:50 +0200

    added new requests pare

--- db.py
+++ db.py
@@ -1,5 +1,11 @@
 import sqlite3
 
+def error_show(msg):
+    date = {
+        "message" : msg
+    }
+    return date
+
 def formate_data(data):
     ar = []
     for i in range(0, len(data)):
@@ -18,8 +24,8 @@ def formate_data_exchange(data):
     for i in range(0, len(data)):
         date = {
             "Id" : data[i][0],
-            "BaseCurrencyId" : Db1.show_currency_id(data[i][1]),
-            "TargetCurrencyId" : Db1.show_currency_id(data[i][2]),
+            "BaseCurrencyId" : Db1.show_currency_by_id(data[i][1]),
+            "TargetCurrencyId" : Db1.show_currency_by_id(data[i][2]),
             "Rate" : data[i][3]
         }
         ar.append(date)
@@ -31,7 +37,7 @@ class Db:
         self.cursor = self.conn.cursor()
 
     def show_currency(self):
-        currency = self.cursor.execute(f"SELECT * FROM `Currencies`")
+        currency = self.cursor.execute(f"SELECT * FROM `Currencies` group by id")
         return formate_data(currency.fetchall())
     
     def show_exchange(self):
@@ -42,10 +48,27 @@ class Db:
         currency = self.cursor.execute(f"SELECT * FROM `Currencies` WHERE code = ?", (code,))
         return formate_data(currency.fetchall())
     
-    def show_currency_id(self, code):
+    def show_currency_by_id(self, code):
         currency = self.cursor.execute(f"SELECT * FROM `Currencies` WHERE id = ?", (code,))
         return formate_data(currency.fetchall())
-    #   self.cursor.execute("UPDATE `currency` SET `user_balance` = ? WHERE `user_id` = ?",(user_balance,user_id))
+    
+    def show_currency_id_code(self, code):
+        currency = self.cursor.execute(f"SELECT id FROM `Currencies` WHERE code = ?", (code,))
+        result = currency.fetchall()[0][0]
+        return result
+    
+    def show_exchange_id(self, code_first, code_second):
+        currency = self.cursor.execute(f"SELECT * FROM `ExchangeRates` WHERE BaseCurrencyId = ? AND TargetCurrencyId = ?", (code_first,code_second,))
+        pare = currency.fetchall()
+        if len(pare):
+            result = a[0][0]
+        else:
+            result = 0
+        return result
+    
+    def show_exchange_pare_id(self, code):
+        currency = self.cursor.execute(f"SELECT * FROM `ExchangeRates` WHERE id = ?", (code,))
+        return currency.fetchall()
     
     def add_currency(self, code, fullname, sign):
         result = self.cursor.execute("INSERT INTO `Currencies` (`code`,`fullname`, `sign`) VALUES (?,?,?)",(code,fullname,sign))
@@ -53,6 +76,10 @@ class Db:
     
     def add_currency_rate(self, id1, id2, rate):
         result = self.cursor.execute("INSERT INTO `ExchangeRates` (`basecurrencyid`,`targetcurrencyid`, `rate`) VALUES (?,?,?)",(id1,id2,rate))
+        return self.conn.commit()
+    
+    def show_exchange_pare(self, code):
+        currency = self.cursor.execute(f"SELECT * FROM `Currencies` WHERE id = ?", (code,))
         return self.conn.commit()
     
     def close(self):
--- rutes.py
+++ rutes.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.errorhandler(404)
 def error(error):
     return jsonify(
-        massage = "Page not found"
+        message = "Page not found"
     ), 404
 
 @app.route('/currency_add', methods=['POST'])
@@ -32,7 +32,7 @@ def currency():
     Db1 = Db("currency.db")
     return jsonify(Db1.show_currency()), 200
 
-@app.route('/exchange_rates', methods=['GET'])
+@app.route('/exchange', methods=['GET'])
 def exchange_rates():
     Db1 = Db("currency.db")
     return jsonify(Db1.show_exchange()), 200
@@ -40,4 +40,20 @@ def exchange_rates():
 @app.route('/currency/<code>', methods=['GET'])
 def currency_path(code):
     Db1 = Db("currency.db")
-    return jsonify(Db1.show_currency_url(code)) if Db1.show_currency_url(code) else "Currency not exist", 200 if Db1.show_currency_url(code) else 404
+    return jsonify(Db1.show_currency_url(code)) if Db1.show_currency_url(code) else error_show("Currency not exist"), 200 if Db1.show_currency_url(code) else 404
+
+@app.route('/exchange/<code>', methods=['GET'])
+def exchange_path(code):
+    Db1 = Db("currency.db")
+    print(len(code))
+    if len(code) == 6:
+        first_code = Db1.show_currency_id_code(code[0:3])
+        second_code = Db1.show_currency_id_code(code[3:6])
+        rate = Db1.show_exchange_pare_id(Db1.show_exchange_id(first_code,second_code))
+    else:
+        first_code = 0
+        second_code = 0
+        rate = 0
+    return jsonify(
+        formate_data_exchange(rate) if rate else error_show("This pares not existed")
+    ), 200 if rate else 404
